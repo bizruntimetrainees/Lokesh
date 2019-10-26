4f651e781d718a2a1f482c5c68102cd7b3a7db31
Lokesh <lokesh.c@bizruntime.com>
2019-10-26 09:49:41 +0530

    Addo.net and ASP.net

---
+++ 25-10-2019/AdoApp/AdoApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace AdoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConString = @"Data Source=DESKTOP-4VSIHKJ;Initial Catalog=LOKESH;Integrated Security=True";
+            SqlConnection con = new SqlConnection(ConString);
+            string querystring = "select * from Customers";
+            
+            con.Open();
+            
+            SqlCommand cmd = new SqlCommand(querystring, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " +reader[3].ToString());
+            }
+            con.Close();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
